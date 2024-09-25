aecbd14a69ca7e2cb53bc5148e1aa444b4a413ca
Estelle <estelleboo94@gmail.com>
2024-09-25 23:10:29 +0200

    First experiments with adding Product class. First iteration of main menu.

---
+++ Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kassasystem
+{
+    internal class Product
+    {
+        int _productId;
+        string _productName;
+        int _price;
+        bool _soldByKilo;
+
+        public Product(int productId, string productName, int price, bool soldByKilo)
+        {
+            _productId = productId;
+            _productName = productName;
+            _price = price;
+            _soldByKilo = soldByKilo;
+        }
+    }
+}
